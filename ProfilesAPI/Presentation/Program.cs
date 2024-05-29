ba62db140b46a07b37de68cbaee64044fc278df8
pasuii18 <heakaru777@gmail.com>
2024-05-29 12:56:58 +0300

    Initial ProfilesAPI commit

---
+++ ProfilesAPI/Presentation/Program.cs
@@ -0,0 +1,28 @@
+namespace ProfilesAPI;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddAuthorization();
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+        
+        app.Run();
+    }
+}
