be1be443e60e2fdf43f68234230248fc4de2fd44
pasuii18 <heakaru777@gmail.com>
2024-06-07 11:23:23 +0300

    Initial ServiceAPI commit

---
+++ ServicesAPI/Presentation/Common/Middlewares/CustomExceptionHandlerMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace ServicesAPI.Common.Middlewares;
+
+public class CustomExceptionHandlerMiddleware(
+    RequestDelegate next)
+{
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception e)
+        {
+            await HandleExceptionAsync(context, e);
+        }
+    }
+
+    private Task HandleExceptionAsync(HttpContext context, Exception e)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        
+        return context.Response.WriteAsync(e.Message);
+    }
+}
---
+++ ServicesAPI/Presentation/Common/Middlewares/CustomExceptionHandlerMiddlewareExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace ServicesAPI.Common.Middlewares;
+
+public static class CustomExceptionHandlerMiddlewareExtensions
+{
+    public static IApplicationBuilder UseCustomExceptionsHandler(
+        this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<CustomExceptionHandlerMiddleware>();
+    }
+}
