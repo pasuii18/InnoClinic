f9c97e2df368b4d8b06f56711d1a31d4132a2e66
pasuii18 <heakaru777@gmail.com>
2024-06-19 11:18:43 +0300

    add: migrations context

---
+++ AppointmentsAPI/Infrastructure/Persistence/Contexts/MigrationsDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Domain.Entities;
+using Infrastructure.Common.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Infrastructure.Persistence.Contexts;
+
+public class MigrationsDbContext(DbContextOptions<MigrationsDbContext> options,
+    IOptions<PostgresDbOptions> sqlOptions)
+    : DbContext(options)
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        => optionsBuilder.UseNpgsql(sqlOptions.Value.PostgresConnectionString);
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+}
