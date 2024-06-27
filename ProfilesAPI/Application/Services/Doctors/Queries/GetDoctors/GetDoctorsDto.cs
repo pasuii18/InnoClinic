e633670072146e628d36da26752a5bc17350bf3b
pasuii18 <heakaru777@gmail.com>
2024-06-27 15:28:03 +0300

    chore: new queries dtos

---
+++ ProfilesAPI/Application/Services/Doctors/Queries/GetDoctors/GetDoctorsDto.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+
+namespace Application.Common.Dtos.DoctorDtos;
+
+public record GetDoctorsDto(IReadOnlyCollection<GetDoctorProfileDto> Doctors)
+{
+    public static GetDoctorsDto MapFromDoctors(IReadOnlyCollection<Doctor> doctors)
+    {
+        return new GetDoctorsDto(doctors.Select(GetDoctorProfileDto.MapFromDoctor).ToList().AsReadOnly());
+    }
+}
---
+++ ProfilesAPI/Application/Services/Receptionists/Queries/GetReceptionists/GetReceptionistsDto.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+
+namespace Application.Common.Dtos.ReceptionistDtos;
+
+public record GetReceptionistsDto(IReadOnlyCollection<GetReceptionistProfileDto> Receptionists)
+{
+    public static GetReceptionistsDto MapFromReceptionist(IReadOnlyCollection<Receptionist> receptionists)
+    {
+        return new GetReceptionistsDto(
+            receptionists.Select(GetReceptionistProfileDto.MapFromReceptionist).ToList().AsReadOnly());
+    }
+}
